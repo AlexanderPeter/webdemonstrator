304123055f842267a0b675ffab0968134094468b
Alexander Peter <alexander.peter@stud.hslu.ch>
2024-01-14 17:50:43 +0100

    add streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,113 @@
+import cv2
+import numpy as np
+import streamlit as st
+from ultralytics import YOLO
+
+title = "Dental charting automation from panoramic x-rays"
+shift = 1
+models = {"../models/yolov8x-seg-dentex-diseases.pt": None}
+
+
+def create_opencv_image_from_stringio(img_stream, cv2_img_flag=1):
+    img_stream.seek(0)
+    img_array = np.asarray(bytearray(img_stream.read()), dtype=np.uint8)
+    return cv2.imdecode(img_array, cv2_img_flag)
+
+
+def predict_yolo(model_path, image, threshold=0.5):
+    if models[model_path] is None:
+        models[model_path] = YOLO(model_path)
+    outputs = models[model_path].predict(image)
+    classes = outputs[0].boxes.cls.tolist()
+    scores = outputs[0].boxes.conf.tolist()
+    boxes = outputs[0].boxes.xyxy.tolist()
+    masks = (
+        [mask.tolist() for mask in outputs[0].masks.xy]
+        if outputs[0].masks is not None
+        else []
+    )
+    labels = []
+    polygons = []
+    for score, label, box, mask in zip(scores, classes, boxes, masks):
+        if threshold < score:
+            if mask is None:
+                polygons.append(
+                    [
+                        (box[0], box[1]),
+                        (box[2], box[1]),
+                        (box[2], box[3]),
+                        (box[0], box[3]),
+                    ]
+                )
+            else:
+                polygons.append(mask)
+            labels.append(int(label + shift))
+    return polygons, labels
+
+
+def draw_instances(
+    img,
+    polygons,
+    labels,
+    color_polygon=(0, 0, 220),
+    color_font=(255, 255, 255),
+    thickness=3,
+    font_size=1.5,
+    font=cv2.FONT_HERSHEY_SIMPLEX,
+    alpha=0.15,
+):
+    overlay = img.copy()
+    for polygon, label in zip(polygons, labels):
+        coords = np.int32([polygon])
+        cv2.polylines(img, np.int32([coords]), True, color_polygon, thickness=thickness)
+        cv2.fillPoly(
+            overlay, np.int32([coords]), color=color_polygon, lineType=cv2.LINE_AA
+        )
+        x_list = [coord[0] for coord in polygon]
+        y_list = [coord[1] for coord in polygon]
+        x_min = min(x_list)
+        y_min = min(y_list)
+        x_max = max(x_list)
+        y_max = max(y_list)
+        width = x_max - x_min
+        height = y_max - y_min
+        text = str(label)
+        textsize = cv2.getTextSize(text, font, font_size, thickness)[0]
+        cv2.putText(
+            img,
+            text,
+            (
+                int(x_min + width / 2 - textsize[0] / 2),
+                int(y_min + height / 2 - textsize[1] / 2),
+            ),
+            font,
+            font_size,
+            color_font,
+            thickness,
+        )
+    img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
+    return img
+
+
+st.set_page_config(page_title=title, page_icon="🦷")
+st.title(title)
+
+img_file_buffer = st.file_uploader(
+    "Choose image file to detect",
+    type=["png", "jpg", "jpeg"],
+    accept_multiple_files=False,
+)
+if img_file_buffer is not None:
+    open_cv_image = create_opencv_image_from_stringio(img_file_buffer)
+
+    polygons, labels = predict_yolo(list(models.keys())[0], open_cv_image)
+    img = draw_instances(
+        open_cv_image.copy(), polygons, labels, color_polygon=(220, 0, 0)
+    )
+    st.image(
+        img,
+        caption=[
+            "DENTEX-diseases: 1=Impacted, 2=Caries, 3=Periapical Lesion, 4=Deep Caries",
+        ],
+        channels="BGR",
+    )
